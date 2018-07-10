4a2c17df1afae1a79e3626f28ad1bc0227f6763e
Markell Owens <markellowens@mac.com>
2018-07-10 03:38:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function prependKitten(name) {
   return [name, ...kittens];
 }
 function removeLastKitten () {
-  kittens.slice(0, -1);
+  kittens.slice(0, kittens.length -1);
 }
 function removeFirstKitten () {
   kittens.slice(1);
