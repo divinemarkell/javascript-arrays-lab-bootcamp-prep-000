24a48ccbe87660c5fbc6f4e062a66b6994a622a8
Markell Owens <markellowens@mac.com>
2018-07-10 03:06:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Add your functions and code here
-function destructivelyAppendKitten (array) {
-  array.unshift();
+function destructivelyAppendKitten (array, element) {
+  array.push(element);
   return array
 }
