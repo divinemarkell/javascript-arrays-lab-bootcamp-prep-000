9ee161c26213c471e0c28e12771d00d5055631b9
Markell Owens <markellowens@mac.com>
2018-07-10 03:10:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Add your functions and code here
-function destructivelyAppendKitten (array, element) {
-  array.push(element);
-  return array
+function destructivelyAppendKitten (kittens) {
+  return kittens.push();
 }
